577caf882163688ad79b4ea1b656585ae2fd9846
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-18 17:20:14 +0200

    [UPD] passaggi base express

---
+++ app.js
@@ -0,0 +1,18 @@
+// importo express
+const express = require("express");
+
+// creo l'istanza dell'appa attraverso il mertodo express che ho importato
+const app = express();
+
+// definisco il numero della porto sulla quale far girare l'applicazione
+const port = 3000;
+
+// definisco la rotta base
+app.get("/", (req, res) => {
+  res.send("Rotta base del mio blog");
+});
+
+// dico al server di rimanere in ascolto sulla porta 3000
+app.listen(port, () => {
+  console.log(`Server in ascolto sulla porta ${port}`);
+});
