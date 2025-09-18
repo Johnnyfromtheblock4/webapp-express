7a8aeac90cd63ec1cbcfee1501abb15bc114daa5
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-18 19:32:07 +0200

    [UPD] creazione movieController

---
+++ routers/movieRouter.js
@@ -0,0 +1,9 @@
+// importiamo express
+const express = require("../data/express");
+
+// importo il router
+const router = express.Router();
+
+// improto il controller
+const movieController = require("../controllers/movieController");
+
