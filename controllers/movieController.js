b462ded824eeafe4469fb17e02c7b1571cc6a846
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-18 19:56:06 +0200

    [UPD] show

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,8 +3,10 @@ const connection = require("../data/db");
 
 // index
 const index = (req, res) => {
+  // creo la query
   const sql = "SELECT * FROM movies";
 
+  // eseguo la query
   connection.query(sql, (err, results) => {
     if (err)
       return res
@@ -16,7 +18,20 @@ const index = (req, res) => {
 
 // show
 const show = (req, res) => {
-  console.log("Metodo show");
+  // recupero l'id parametro
+  const { id } = req.params;
+
+  // creo la query
+  const sqlBook = "SELECT * FROM movies WHERE id = ?";
+
+  // eseguo la query passando ora i parametri
+  connection.query(sqlBook, [id], (err, resultBook) => {
+    if (err)
+      return res.status(500).json({
+        error: `errore nell'esecuzione della query: ${err}`,
+      });
+    res.send(resultBook[0]);
+  });
 };
 
 module.exports = {
