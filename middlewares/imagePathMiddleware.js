ecf358a1e782027cc4938370faad904dd028a566
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-22 15:38:40 +0200

    [UPD] refuso imagePath

--- middlewares/imagePathMiddleware.js
+++ middlewares/imagePathMiddleware.js
@@ -1,5 +1,5 @@
 const setImagePath = (req, res, next) => {
-  req.ImagePath = `${req.protocol}://${req.get("host")}/imgs/movies_cover/`;
+  req.imagePath = `${req.protocol}://${req.get("host")}/imgs/movies_cover/`;
   next();
 };
 
